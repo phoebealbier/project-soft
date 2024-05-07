385dfb45a0b4c731e2a707562eba9cf33f6c5b0a
asmaaedris <153117582+asmaaedris@users.noreply.github.com>
2024-05-07 22:20:44 +0300

    project-soft

---
+++ foodOrdering/src/app/(tabs)/menu/index.tsx
@@ -0,0 +1,23 @@
+import { FlatList } from 'react-native';
+
+import products from '@assets/data/products';
+import ProductListItem from '@components/ProductListItem';
+
+    
+
+export default function MenueScreen() {
+  return (
+   
+   
+   <FlatList
+  data={products}
+  renderItem={({ item }) => <ProductListItem product={item} />}
+  numColumns={2}
+  contentContainerStyle={{ gap: 10, padding: 10 }}
+  columnWrapperStyle={{ gap: 10 }}
+/>
+   
+      
+  );
+}
+
---
+++ foodOrdering/src/components/ProductListItem.tsx
@@ -0,0 +1,55 @@
+import { StyleSheet, Image, Text, View ,Pressable} from 'react-native';
+import Colors from '../constants/Colors';
+import {Product} from '../types';
+import { Link } from 'expo-router';
+export const defaultPizzaImage =
+'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+type ProductListItemProps = {
+    product: Product;
+  };
+const ProductListItem = ({product}:ProductListItemProps) => {
+	return ( <Link href={`/(tabs)/menu/${product.id}`} asChild>
+    
+      <Pressable style={styles.container}>
+
+    <Image
+      source={{ uri: product.image ||  defaultPizzaImage }}
+      style={styles.image}
+      resizeMode="contain"
+    />
+    <Text style={styles.title}>{product.name}</Text>
+    <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+  
+ </Pressable>
+
+ </Link>
+  );
+};
+
+export default ProductListItem ;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 10,
+    overflow: 'hidden',
+    flex:1,
+  
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+  },
+  title: {
+    fontWeight: '600',
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+    marginTop: 'auto',
+  },
+});
